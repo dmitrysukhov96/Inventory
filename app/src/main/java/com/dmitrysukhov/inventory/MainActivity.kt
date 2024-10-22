dbfad34d923bacced92072ace5fe683b355ef7b6
dima <dmitrysukhov96@gmail.com>
2024-10-22 21:35:22 +0300

    почистил

--- app/src/main/java/com/dmitrysukhov/inventory/MainActivity.kt
+++ app/src/main/java/com/dmitrysukhov/inventory/MainActivity.kt
@@ -3,10 +3,6 @@ package com.dmitrysukhov.inventory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -20,13 +16,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -68,63 +60,13 @@ fun MyApp() {
     })
     { padding ->
         NavHost(
-            navController = navController, startDestination = MAIN_SCREEN,
+            navController = navController, startDestination = INVENTORY_SCREEN,
             modifier = Modifier.padding(padding)
         ) {
-            composable(MAIN_SCREEN) { MainScreen(navController) }
-
-            composable(CITY_SCREEN) { CityScreen(navController) }
-
-            composable(TEST_SCREEN) { TestScreen(navController) }
-
             composable(INVENTORY_SCREEN) { InventoryScreen() }
             composable(ADD_ITEM_SCREEN) { AddItemScreen() }
             composable(EDIT_ITEM_SCREEN) { EditItemScreen() }
             composable(ITEM_DETAILS_SCREEN) { ItemDetailsScreen() }
-            composable(SCREEN_HOUSE) { ScreenHouse(navController) }
-            composable(SCREEN_ATB) { ScreenAtb(navController) }
-            composable(SCREEN_HOME) { ScreenHome(navController) }
-            composable(SCREEN_WALK) { ScreenWalk(navController) }
-            composable(FON_SCREEN) { FonScreen() }
-            composable(SVETOFOR_SCREEN) { SvetoforScreen() }
-            composable(PRIMER_SCREEN) { PrimerScreen() }
-            composable(LIST_SCREEN) { ListScreen() }
-        }
-    }
-}
-
-val MAIN_SCREEN = "Main screen"
-
-@Composable
-fun MainScreen(navController: NavHostController) {
-    Box(Modifier.fillMaxSize()) {
-        Text(text = "Привет Костя", Modifier.align(Alignment.BottomStart).padding(16.dp), fontSize = 20.sp)
-    }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Button(onClick = { navController.navigate(INVENTORY_SCREEN) }) {
-            Text(text = "Инвентарь")
-        }
-        Button(onClick = { navController.navigate(CITY_SCREEN) }) {
-            Text(text = "Ваш город")
-        }
-        Button(onClick = { navController.navigate(SCREEN_HOUSE) }) {
-            Text(text = "Гуляние")
-        }
-        Button(onClick = { navController.navigate(TEST_SCREEN) }) {
-            Text(text = "Тестовый экран")
-        }
-        Button(onClick = { navController.navigate(FON_SCREEN) }) {
-            Text(text = "Смена фона")
-        }
-        Button(onClick = { navController.navigate(SVETOFOR_SCREEN) }) {
-            Text(text = "Светофор")
-        }
-        Button(onClick = { navController.navigate(LIST_SCREEN) }) {
-            Text(text = "Список")
         }
     }
 }
